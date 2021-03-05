3be5ac840e2aba586096a52619d2bc5b3761a705
Franz Heinzmann (Frando) <frando@unbiskant.org>
2021-03-05 14:34:06 +0100

    Implement AsyncRead and AsyncWrite for Extension

--- src/extension.rs
+++ src/extension.rs
@@ -1,9 +1,15 @@
+use crate::constants::MAX_MESSAGE_SIZE;
 use crate::message::{ChannelMessage, ExtensionMessage, Message};
 use crate::schema::*;
 use async_channel::{Receiver, Sender};
-use futures_lite::stream::Stream;
+use futures_lite::{ready, AsyncRead, AsyncWrite, FutureExt, Stream};
 use std::collections::HashMap;
+use std::future::Future;
+use std::io;
 use std::pin::Pin;
+use std::task::{Context, Poll};
+
+const MAX_BODY_SIZE: usize = MAX_MESSAGE_SIZE as usize - 16;
 
 #[derive(Debug)]
 pub struct Extensions {
@@ -47,6 +53,8 @@ impl Extensions {
             local_id,
             outbound_tx: self.outbound_tx.clone(),
             inbound_rx,
+            write_state: WriteState::Idle,
+            read_state: None,
         };
         self.extensions.insert(name, handle);
 
@@ -95,6 +103,52 @@ pub struct Extension {
     local_id: u64,
     outbound_tx: Sender<ChannelMessage>,
     inbound_rx: Receiver<Vec<u8>>,
+    write_state: WriteState,
+    read_state: Option<Vec<u8>>,
+}
+
+type SendFuture = Pin<Box<dyn Future<Output = io::Result<()>> + Send + Sync + 'static>>;
+
+enum WriteState {
+    Sending(SendFuture, usize),
+    Idle,
+}
+
+impl std::fmt::Debug for WriteState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            WriteState::Sending(_, len) => {
+                write!(f, "Sending(len={})", len)
+            }
+            WriteState::Idle => write!(f, "Idle"),
+        }
+    }
+}
+
+impl Extension {
+    pub async fn send(&self, message: Vec<u8>) {
+        let message = ExtensionMessage::new(self.local_id, message);
+        let message = ChannelMessage::new(self.channel, Message::Extension(message));
+        self.outbound_tx.send(message).await.unwrap()
+    }
+
+    pub fn send_pinned(&self, message: Vec<u8>) -> SendFuture {
+        let message = ExtensionMessage::new(self.local_id, message);
+        let message = ChannelMessage::new(self.channel, Message::Extension(message));
+        // TODO: It would be nice to do this without cloning, but I didn't find a way so far.
+        let fut = send_message(self.outbound_tx.clone(), message);
+        Box::pin(fut)
+    }
+}
+
+pub async fn send_message(
+    sender: Sender<ChannelMessage>,
+    message: ChannelMessage,
+) -> io::Result<()> {
+    sender
+        .send(message)
+        .await
+        .map_err(|e| io::Error::new(io::ErrorKind::Interrupted, format!("Channel error: {}", e)))
 }
 
 impl Stream for Extension {
@@ -107,10 +161,59 @@ impl Stream for Extension {
     }
 }
 
-impl Extension {
-    pub async fn send(&mut self, message: Vec<u8>) {
-        let message = ExtensionMessage::new(self.local_id, message);
-        let message = ChannelMessage::new(self.channel, Message::Extension(message));
-        self.outbound_tx.send(message).await.unwrap()
+impl AsyncRead for Extension {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<io::Result<usize>> {
+        let mut this = self.get_mut();
+        let message = if let Some(message) = this.read_state.take() {
+            message
+        } else {
+            let message = ready!(Pin::new(&mut this).poll_next(cx));
+            message.ok_or_else(|| io::Error::new(io::ErrorKind::Interrupted, "Channel closed"))?
+        };
+        let len = message.len().min(buf.len());
+        buf[..len].copy_from_slice(&message[..len]);
+        if message.len() > len {
+            this.read_state = Some(message[len..].to_vec());
+        } else {
+            this.read_state = None
+        }
+        Poll::Ready(Ok(len))
+    }
+}
+
+impl AsyncWrite for Extension {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        let this = self.get_mut();
+        loop {
+            match this.write_state {
+                WriteState::Idle => {
+                    let len = buf.len().min(MAX_BODY_SIZE);
+                    let fut = this.send_pinned(buf.to_vec());
+                    this.write_state = WriteState::Sending(fut, len);
+                }
+                WriteState::Sending(ref mut fut, len) => {
+                    let res = ready!(fut.poll(cx));
+                    let res = res.map(|_| len);
+                    this.write_state = WriteState::Idle;
+                    return Poll::Ready(res);
+                }
+            }
+        }
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_close(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        Poll::Ready(Ok(()))
     }
 }
--- tests/extension.rs
+++ tests/extension.rs
@@ -4,6 +4,7 @@ use async_std::net::TcpStream;
 use async_std::prelude::*;
 use async_std::task::{self, JoinHandle};
 use futures_lite::io::{AsyncRead, AsyncWrite};
+// use futures_lite::{AsyncReadExt, AsyncWriteExt};
 use hypercore_protocol::schema::*;
 use hypercore_protocol::{discovery_key, Channel, Event, Message, Protocol, ProtocolBuilder};
 use std::io;
@@ -18,6 +19,19 @@ where
 {
     task::spawn(async move { while let Some(_event) = proto.next().await {} })
 }
+
+// Drive a number of streams to completion.
+// fn drive_all<S>(streams: Vec<S>) -> JoinHandle<()>
+// where
+//     S: Stream + Send + Unpin + 'static,
+// {
+//     let join_handles = streams.into_iter().map(drive);
+//     task::spawn(async move {
+//         for join_handle in join_handles {
+//             join_handle.await;
+//         }
+//     })
+// }
 
 // Drive a protocol stream until the first channel arrives.
 fn drive_until_channel<R, W>(
@@ -102,5 +116,61 @@ async fn channel_extension() -> anyhow::Result<()> {
     let response = ext_a.next().await;
     assert_eq!(response, Some(b"ack".to_vec()));
     // eprintln!("A received: {:?}", response.map(String::from_utf8));
+    Ok(())
+}
+
+#[async_std::test]
+async fn channel_extension_async_read_write() -> anyhow::Result<()> {
+    // env_logger::init();
+    let (mut proto_a, mut proto_b) = create_pair_memory().await?;
+    let key = [1u8; 32];
+
+    proto_a.open(key).await?;
+    proto_b.open(key).await?;
+
+    let next_a = drive_until_channel(proto_a);
+    let next_b = drive_until_channel(proto_b);
+    let (proto_a, mut channel_a) = next_a.await?;
+    let (proto_b, mut channel_b) = next_b.await?;
+
+    let mut ext_a = channel_a.register_extension("ext");
+    let mut ext_b = channel_b.register_extension("ext");
+
+    drive(proto_a);
+    drive(proto_b);
+    drive(channel_a);
+    drive(channel_b);
+
+    task::spawn(async move {
+        let mut read_buf = vec![0u8; 3];
+        // let mut total = 0;
+        let mut res = vec![];
+        while res.len() < 10 {
+            let n = ext_b.read(&mut read_buf).await.unwrap();
+            // eprintln!(
+            //     "B read: n {} buf {}",
+            //     n,
+            //     std::str::from_utf8(&read_buf[..n]).unwrap()
+            // );
+            res.extend_from_slice(&read_buf[..n]);
+        }
+        assert_eq!(res, b"helloworld".to_vec());
+
+        let write = b"ack".to_vec();
+        ext_b.write_all(&write).await.unwrap();
+    });
+
+    ext_a.write_all(b"hello").await.unwrap();
+    ext_a.write_all(b"world").await.unwrap();
+
+    let mut read_buf = vec![0u8; 5];
+    let n = ext_a.read(&mut read_buf).await.unwrap();
+    assert_eq!(n, 3);
+    assert_eq!(&read_buf[..n], b"ack");
+    // eprintln!(
+    //     "A read: n {} buf {}",
+    //     n,
+    //     std::str::from_utf8(&read_buf[..n]).unwrap()
+    // );
     Ok(())
 }
