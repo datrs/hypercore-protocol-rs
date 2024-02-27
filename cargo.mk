52c3ad7e9553aeab3183d7dbbb885133e584f783
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-02-27 18:25:03 -0500

    cargo.mk:remove . $(HOME)/.cargo/env

--- cargo.mk
+++ cargo.mk
@@ -5,30 +5,23 @@ cargo-help:### 	cargo-help
 cargo-b:cargo-build### 	cargo b
 cargo-build:### 	cargo build
 ## 	make cargo-build q=true
-	@. $(HOME)/.cargo/env
 	@RUST_BACKTRACE=all cargo b $(QUIET)
 cargo-i:cargo-install
 cargo-install:### 	cargo install --path .
-	#@. $(HOME)/.cargo/env
 	@cargo install --path $(PWD)
 cargo-br:cargo-build-release### 	cargo-br
 ## 	make cargo-br q=true
 cargo-b-release:cargo-build-release### 	cargo-build-release
 cargo-build-release:### 	cargo-build-release
 ## 	make cargo-build-release q=true
-	@. $(HOME)/.cargo/env
 	@cargo b --release $(QUIET)
 cargo-check:### 	cargo-check
-	@. $(HOME)/.cargo/env
 	@cargo c
 cargo-bench:### 	cargo-bench
-	@. $(HOME)/.cargo/env
 	@cargo bench
 cargo-test:### 	cargo-test
-	@. $(HOME)/.cargo/env
 	@cargo test
 cargo-report:### 	cargo-report
-	@. $(HOME)/.cargo/env
 	cargo report future-incompatibilities --id 1
 
 cargo-sort:## 	cargo-sort
